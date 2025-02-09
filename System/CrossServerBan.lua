be7fd691bfa275618c3cbf11d90b49bfcae26003
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:46:33 -0500

    Create CrossServerBan.lua

---
+++ System/CrossServerBan.lua
@@ -0,0 +1,8 @@
+-- CrossServerBan.lua
+local function banPlayerAcrossServers(player)
+    -- Logic for banning the player across all game servers
+end
+
+return {
+    banPlayerAcrossServers = banPlayerAcrossServers
+}
