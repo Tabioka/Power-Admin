b5ed9d3e7a19ab6c13a8abed30a8651b0eb296d1
Tabioka <donovanbrady102@gmail.com>
2025-02-09 07:31:29 -0500

    Update AdminRoles.lua

--- src/AdminRoles.lua
+++ src/AdminRoles.lua
@@ -1,1 +1,10 @@
+-- AdminRoles.lua
+local roles = {
+    Owner = { permissions = {"all"} },
+    Admin = { permissions = {"ban", "kick", "mute"} },
+    Mod = { permissions = {"mute", "warn"} },
+    VIP = { permissions = {"useVIPRoom"} }
+}
+
+return roles
 
