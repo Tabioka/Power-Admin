28525e776366f8370dec80170405e5c0ace50990
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:50:05 -0500

    Create GroupIntegration.lua

---
+++ Integration/GroupIntegration.lua
@@ -0,0 +1,8 @@
+-- GroupIntegration.lua
+local function assignGroupRole(player)
+    -- Check if player is part of a group and assign permissions based on group rank
+end
+
+return {
+    assignGroupRole = assignGroupRole
+}
