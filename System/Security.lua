03b0b0877dc891c72428999c523c5dfe758bb39d
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:44:50 -0500

    Create Security.lua

---
+++ System/Security.lua
@@ -0,0 +1,8 @@
+-- Security.lua
+local function checkForExploiters(player)
+    -- Basic checks for exploiters (e.g., unusual stats, speed)
+end
+
+return {
+    checkForExploiters = checkForExploiters
+}
