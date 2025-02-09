7f8f4982a1e1643fbc72e865acb001922578b297
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:53:52 -0500

    Create PermissionsConfig.lua

---
+++ Config/PermissionsConfig.lua
@@ -0,0 +1,9 @@
+-- PermissionsConfig.lua
+local permissions = {
+    ban = {"Owner", "Admin"},
+    kick = {"Owner", "Admin", "Mod"},
+    mute = {"Owner", "Admin", "Mod"},
+    teleport = {"Owner", "Admin", "Mod", "VIP"}
+}
+
+return permissions
