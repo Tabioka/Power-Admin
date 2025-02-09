2e459a2a9cec765ab054c1e141f1cf4c97d2ef41
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:25:31 -0500

    Create FunCommands.lua

---
+++ commands/FunCommands.lua
@@ -0,0 +1,10 @@
+-- FunCommands.lua
+local BaseCommands = require(game.ServerScriptService:WaitForChild("Power-Admin"):WaitForChild("src"):WaitForChild("Commands"):WaitForChild("BaseCommands"))
+
+local function giveDance(player)
+    -- Add dance animation or item to player
+end
+
+return {
+    giveDance = giveDance
+}
