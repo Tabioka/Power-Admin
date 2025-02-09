23324b163e69496ee67face9389abbca72005b92
Tabioka <donovanbrady102@gmail.com>
2025-02-09 07:27:55 -0500

    Update CustomUI.lua

--- src/CustomUI.lua
+++ src/CustomUI.lua
@@ -1,1 +1,12 @@
+-- CustomUI.lua
+local function createButton(text, action)
+    local button = Instance.new("TextButton")
+    button.Text = text
+    button.MouseButton1Click:Connect(action)
+    return button
+end
+
+return {
+    createButton = createButton
+}
 
