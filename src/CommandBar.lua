5b429db0da4076a998c94cb9c1b676fd129a9a8f
Tabioka <donovanbrady102@gmail.com>
2025-02-09 07:21:55 -0500

    Update CommandBar.lua

--- src/CommandBar.lua
+++ src/CommandBar.lua
@@ -1,1 +1,12 @@
+-- CommandBar.lua
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local commandBar = script.Parent
+
+local function handleCommand(player, commandText)
+    -- Handle and validate commands
+end
+
+return {
+    handleCommand = handleCommand
+}
 
