4e5e4993cc5360c6766c786697aaa956464a4e9b
Tabioka <donovanbrady102@gmail.com>
2025-02-09 07:18:29 -0500

    Update MainAdminPanel.lua

--- src/MainAdminPanel.lua
+++ src/MainAdminPanel.lua
@@ -1,1 +1,14 @@
+-- MainAdminPanel.lua
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local AdminPanel = script.Parent
+local CommandBar = require(AdminPanel:WaitForChild("CommandBar"))
+local CustomUI = require(AdminPanel:WaitForChild("CustomUI"))
+
+local function setupAdminPanel(player)
+    -- Admin UI setup
+end
+
+return {
+    setupAdminPanel = setupAdminPanel
+}
 
