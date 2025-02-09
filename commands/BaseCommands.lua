fd4b12bd0d5af17bbe3163f516abebe2f2333af2
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:16:58 -0500

    Update BaseCommands.lua

--- commands/BaseCommands.lua
+++ commands/BaseCommands.lua
@@ -1,1 +1,20 @@
+-- BaseCommands.lua
+local AdminRoles = require(game.ServerScriptService:WaitForChild("Power-Admin"):WaitForChild("src"):WaitForChild("AdminPanel"):WaitForChild("AdminRoles"))
+local PermissionManager = require(game.ServerScriptService:WaitForChild("Power-Admin"):WaitForChild("src"):WaitForChild("System"):WaitForChild("PermissionManager"))
+
+local function executeCommand(player, commandName, args)
+    local role = AdminRoles[player.UserId] or "Guest"
+    local permissions = PermissionManager.getPermissions(role)
+    
+    if not permissions[commandName] then
+        warn(player.Name .. " does not have permission to execute " .. commandName)
+        return
+    end
+
+    -- Execute command logic here
+end
+
+return {
+    executeCommand = executeCommand
+}
 
