93b7522dd17be59b8ee9697fba4b6f0f51a202c8
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:48:07 -0500

    Create RemoteCommandExecutor.lua

---
+++ System/RemoteCommandExecutor.lua
@@ -0,0 +1,8 @@
+-- RemoteCommandExecutor.lua
+local function executeRemoteCommand(command, args)
+    -- Execute a command from an external service (like Discord)
+end
+
+return {
+    executeRemoteCommand = executeRemoteCommand
+}
