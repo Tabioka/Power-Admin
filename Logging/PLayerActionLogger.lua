b795fefc380639d6eac8e3ebc9e7bcad1be0f56f
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:42:32 -0500

    Create PLayerActionLogger.lua

---
+++ Logging/PLayerActionLogger.lua
@@ -0,0 +1,8 @@
+-- PlayerActionLogger.lua
+local function logPlayerAction(player, action)
+    -- Log player-specific action (e.g., killing, using specific tools)
+end
+
+return {
+    logPlayerAction = logPlayerAction
+}
