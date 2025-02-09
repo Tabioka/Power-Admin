8cf84b70881a96df3dc7e48f372e8768b51b09e2
Tabioka <donovanbrady102@gmail.com>
2025-02-09 08:49:08 -0500

    Create DiscordIntegration.lua

---
+++ Integration/DiscordIntegration.lua
@@ -0,0 +1,8 @@
+-- DiscordIntegration.lua
+local function sendDiscordAlert(message)
+    -- Send a message to a Discord channel using webhook or bot
+end
+
+return {
+    sendDiscordAlert = sendDiscordAlert
+}
